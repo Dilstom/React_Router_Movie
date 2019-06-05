eb818649091762dc641ac491abbee4cc5091ddf2
Dil Sombayeva <dilyasom@gmail.com>
2019-06-05 12:55:55 -0700

    console.log props

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,7 +12,7 @@ export default class Movie extends Component {
  componentDidMount() {
   // change this line to grab the id passed on the URL
   const id = this.props.match.params.id;
-  //   console.log('params', this.props);
+  csonsole.log('params', this.props);
   this.fetchMovie(id);
  }
 
