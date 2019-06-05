007b63d04c60510147b2d27ceaf79d66ac1bf343
Dil Sombayeva <dilyasom@gmail.com>
2019-06-05 13:02:42 -0700

    fix typo

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,7 +12,7 @@ export default class Movie extends Component {
  componentDidMount() {
   // change this line to grab the id passed on the URL
   const id = this.props.match.params.id;
-  csonsole.log('params', this.props);
+  console.log('params', this.props);
   this.fetchMovie(id);
  }
 
